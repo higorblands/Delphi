b03b764f1f777ebf62b94ba9548fb031fdc71b99
higorblands <higorblandes@gmail.com>
2020-12-07 14:48:21 -0300

    Atualizando e resolvendo exercicio 4

---
+++ Exercicio 4/Project3.dpr
@@ -0,0 +1,14 @@
+program Project3;
+
+uses
+  Vcl.Forms,
+  Unit5 in 'Unit5.pas' {Form3};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm3, Form3);
+  Application.Run;
+end.
