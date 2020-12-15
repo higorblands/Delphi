4d1efc4dc060d6a10b44a7118caa9812ba4a591a
higorblands <higorblandes@gmail.com>
2020-12-15 09:15:33 -0300

    Atualizando git

--- Exercicio 9/Unit1.pas
+++ Exercicio 9/Unit1.pas
@@ -3,15 +3,16 @@ unit Unit1;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
 
 type
   TForm1 = class(TForm)
     ListBox1: TListBox;
     Edit1: TEdit;
-    Edit2: TEdit;
-    Edit3: TEdit;
+    age: TEdit;
+    weight: TEdit;
     Button1: TButton;
     function validar(a: string): boolean;
     procedure Button1Click(Sender: TObject);
@@ -31,32 +32,32 @@ implementation
 procedure TForm1.Button1Click(Sender: TObject);
 var
   arquivo: TextFile;
-  nameFile,line: string;
+  name, age, wei, nameFile, line: string;
+  i, value, size: integer;
 
 begin
-if (validar(Edit1.Text) = false) then
- begin
+  if (validar(Edit1.Text) = false) then
+  begin
     ShowMessage('Você deixou o campo em branco, não é possível seguir.');
   end
   else
   begin
-   nameFile :=Edit1.Text+'.txt';
+    nameFile := Edit1.Text + '.txt';
     AssignFile(arquivo, nameFile);
-     Reset(arquivo);
+    Reset(arquivo);
     while not Eof(arquivo) do
-        begin
-          Readln(arquivo, line);
-          ListBox1.Items.Add(line);
-        end;
-        closeFile(arquivo);
+    begin
+      Readln(arquivo, line);
+      ListBox1.Items.Add(line);
+    end;
+    closeFile(arquivo);
   end;
-
 end;
 
 function TForm1.validar(a: string): boolean;
 begin
-result := true;
- if (a = '') then
+  result := true;
+  if (a = '') then
   begin
     result := false;
   end;
---
+++ Exercicio 9/__history/Unit1.dfm.~8~
@@ -0,0 +1,57 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 355
+  ClientWidth = 691
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object ListBox1: TListBox
+    Left = 0
+    Top = 152
+    Width = 689
+    Height = 201
+    ItemHeight = 13
+    TabOrder = 0
+  end
+  object Edit1: TEdit
+    Left = 284
+    Top = 32
+    Width = 121
+    Height = 21
+    TabOrder = 1
+    TextHint = 'Nome do arquivo'
+  end
+  object age: TEdit
+    Left = 355
+    Top = 107
+    Width = 74
+    Height = 21
+    TabOrder = 2
+    TextHint = 'Idade'
+  end
+  object weight: TEdit
+    Left = 259
+    Top = 107
+    Width = 74
+    Height = 21
+    TabOrder = 3
+    TextHint = 'Peso'
+  end
+  object Button1: TButton
+    Left = 308
+    Top = 59
+    Width = 75
+    Height = 25
+    Caption = 'Verificar'
+    TabOrder = 4
+    OnClick = Button1Click
+  end
+end
